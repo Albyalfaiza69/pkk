98c6fe5de6737bb132378d29ee497f84ffe0aae6
Albyalfaiza69 <127160822+Albyalfaiza69@users.noreply.github.com>
2023-09-11 14:10:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let displayvalue = '';
+
+function calculate(button){
+    const value = button.innertext;
+
+    if(value === '='){
+        try{
+            displayvalue = eval(displayvalue);
+        } catch(error){
+            displayvalue = 'error';
+        }
+    } else if (value ==='c'){
+        displayvalue = '';
+    } else{
+        displayvalue += value;
+    }
+    document.getElementById('display').value = displayvalue;
+
+}
